22f0fbe9bd98f2ce2a7f4c3b5f3c2c38b960a6fe
Sharon Wasker <sharonwasker@gmail.com>
2021-07-16 03:48:53 +0300

    16.7 form submit and send to mail

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,29 +2,37 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z  from "zod";
+import emailjs from 'emailjs-com';
 
 import "./form.css";
 
 const schema = z.object({
     firstName: z.string().min(2, { message: "First name should be at least 2 characters" }).refine(s => s.match(/^[-a-zA-Z]*$/)),
     lastName: z.string().min(2, { message: "Last name should be at least 2 characters" }).refine(s => s.match(/^[-a-zA-Z]*$/)),
-    email: z.string().email({ message: "This is an invalid email address" }),
-    phoneNumber:  z.string().length(10, { message: "Phone number should be 10 characters" }).refine(s => s.match(/^[0-9]*$/)),
+    mailAddress: z.string().email({ message: "This is an invalid e-mail address" }),
+    phoneNumber:  z.string().length(1, { message: "Phone number should be 10 characters" }).refine(s => s.match(/^[0-9]*$/)),
 })
 
 export default function Form() {
 
-        const {
-          register,
-          handleSubmit,
-          formState: { errors },
-        } = useForm({
-          resolver: zodResolver(schema),
-        });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(schema),
+    });
 
-    const onSubmit = (data) => {
-        console.log(data);
-    }
+    function sendEmail(e) {
+    
+        emailjs.sendForm('service_0enz2y6', 'template_erk2dls', '#form', 'user_s69tW4xngZ4shJTShUIww')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          document.getElementById("form").reset()
+      }
 
     // const onSubmit = handleSubmit((values) => {
     //     console.log(values)
@@ -32,7 +40,7 @@ export default function Form() {
 
     return (
         <div className="form-contain">
-            <form className="form" onSubmit={handleSubmit(onSubmit)}>
+            <form id="form" className="form" onSubmit={handleSubmit(sendEmail)}>
                 <h1 className="form-title">Jones Form</h1>
 
                 <div className="form-inputs">
@@ -62,15 +70,15 @@ export default function Form() {
                     </div>
 
                     <div className="e-mail-contain">
-                        <label htmlFor="email" className="form-label" >Mail Address</label>
+                        <label htmlFor="mailAddress" className="form-label" >Mail Address</label>
                         <input
-                            type="email"
-                            name="email"
-                            id="email"
+                            type="mailAddress"
+                            name="mailAddress"
+                            id="mailAddress"
                             className="form-input"
-                            {...register('email')}
+                            {...register('mailAddress')}
                         />
-                        <p className="form-error"> { errors.email?.message } </p>
+                        <p className="form-error"> { errors.mailAddress?.message } </p>
 
                     </div>
 
